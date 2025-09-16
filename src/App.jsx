d73bcc48e8f97bcf1b2d4cad8f0d5c20d48dd05f
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-09-16 13:17:35 +0200

    added a link, link is styled as a in css and instead of href it has to the rest is completely the same - opening and closing tags, citations in attribut

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom"; //you write {BrowserRouter as Router}
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"; //you write {BrowserRouter as Router}
 import Home from "./components/Home"
 import NotFound from "./components/NotFound"
 
@@ -8,6 +8,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
+      <Link to="/home">Home</Link>
         <Routes>
           <Route path="/home" element={<Home/>}/>
           <Route path="*" element={<NotFound/>} /> {/* path="*" Default for all non-declared path, you need to import it as component*/}
