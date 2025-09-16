0e417fd77b65c7fff2dfea9c875c69e455da4813
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-09-16 12:00:42 +0200

    home route and default route in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"; //you write {BrowserRouter as Router}
+import Home from "./components/Home"
+import NotFound from "./components/NotFound"
 
 function App() {
 
   return (
     <>
-      <p>
-        Hello
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="*" element={<NotFound/>} /> {/* path="*" Default for all non-declared path, you need to import it as component*/}
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
