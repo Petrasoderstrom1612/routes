9f24fb5b4c24e1e68dba12c6692b25c4b0e03a5d
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-09-17 15:35:20 +0200

    created state, did fetch request and stored data.van in state. As it was an object holding and array I first had to go inside of the object

--- src/components/Vans.jsx
+++ src/components/Vans.jsx
@@ -16,7 +16,12 @@ const Vans = () => {
     <div>
       <p>here come vans</p>
       {vans.map((oneVan => (
+        <div>
         <h2>{oneVan.name}</h2>
+        <h2>${oneVan.price}/day</h2>
+        <img src={oneVan.imageUrl} alt="{oneVan.name}"/>
+        <p>{oneVan.type}</p>
+        </div>
       )))}
     </div>
   )
