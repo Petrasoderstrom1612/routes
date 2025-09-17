c9a1c4a9e7232d2c2e36e5ad2a18a992f3900c5d
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-09-17 14:32:17 +0200

    fetch standart

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"; //you wri
 import Home from "./components/Home"
 import About from "./components/About"
 import NotFound from "./components/NotFound"
+import Vans from "./components/Vans"
 
 function App() {
 
@@ -13,11 +14,13 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
+          <Route path="/vans" element={<Vans/>}/> {/* the route to the component*/}
           <Route path="*" element={<NotFound/>} /> {/* path="*" Default for all non-declared path, you need to import it as component*/}
         </Routes>
       </BrowserRouter>
---
+++ src/components/Vans.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Vans = () => {
+
+    React.useEffect(()=>{
+        fetch(`api/vans`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
+    ,[])
+  return (
+    <div>
+      <p>here come vans</p>
+    </div>
+  )
+}
+
+export default Vans
