359bcb887b89abfc283ccfade2c294facbdb9059
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-09-17 15:12:28 +0200

    fetched names of the vans

--- src/components/Vans.jsx
+++ src/components/Vans.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 
 const Vans = () => {
+    const [vans, setVans] = React.useState([])
 
     React.useEffect(()=>{
         fetch("/api/vans")
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log("vans array", data.vans)
+            setVans(data.vans)
+        })
     },[])
-    
+
   return (
     <div>
       <p>here come vans</p>
+      {vans.map((oneVan => (
+        <h2>{oneVan.name}</h2>
+      )))}
     </div>
   )
 }
