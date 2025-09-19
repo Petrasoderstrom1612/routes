24188ced156459006eee9c5ce68d3766e7a8acbb
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-09-19 10:07:08 +0200

    added fetch for VanDetails using params in the fetch url, also adding error handling for no fetched info with state = ? : loading html

--- src/components/VanDetails.jsx
+++ src/components/VanDetails.jsx
@@ -2,12 +2,32 @@ import React from 'react'
 import {useParams} from "react-router-dom"
 
 const VanDetails = () => {
-    const params = useParams()
-    console.log("params",params) //du måste gå på sidan för att se console.loggen
+  const params = useParams()
+  console.log("params",params) //du måste gå på sidan för att se console.loggen
+
+  const [vanInfo, setVanInfo] = React.useState(null)
+
+  React.useEffect(()=>{
+    fetch(`/api/vans/${params.id}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log("specific van",data)
+      setVanInfo(data.vans)
+    })
+  },[]) //you could have params.id in the dependency arr if there was a link leading to a different id on this page but otherwise this is save, you fetch once
 
   return (
-    <div>
-      van details
+    <div className="van-detail-container"> {/*Handle situation if no id is available, fetch returns null */}
+         {vanInfo ? ( 
+            <div className="van-detail">
+                <img src={vanInfo.imageUrl} />
+                <i className={`van-type ${vanInfo.type} selected`}>{vanInfo.type}</i>
+                <h2>{vanInfo.name}</h2>
+                <p className="van-price"><span>${vanInfo.price}</span>/day</p>
+                <p>{vanInfo.description}</p>
+                <button className="link-button">Rent this van</button>
+            </div>
+        ) : <h2>Loading...</h2>}
     </div>
   )
 }
