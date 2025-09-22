6dcdc5bb11f0b89781c6379e2a2f863b2da03dde
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-09-22 16:39:14 +0200

    ändrade mappen från components till pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"; //you write {BrowserRouter as Router}
-import Home from "./components/Home"
-import About from "./components/About"
-import NotFound from "./components/NotFound"
-import Vans from "./components/Vans"
-import VanDetails from "./components/VanDetails"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import NotFound from "./pages/NotFound"
+import Vans from "./pages/Vans"
+import VanDetails from "./pages/VanDetails"
 
 function App() {
 
