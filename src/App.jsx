3e556f9a9c75b638664787212f9c5f63bf538d51
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-09-22 17:01:25 +0200

    Added Layout as a parent Route to all Route and created a separate component fo it where I also inserted <Outlet/> for all the Route pages and <Header/> that is a new JSX component including the nav that was originally in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"; //you write {BrowserRouter as Router}
+import { BrowserRouter, Routes, Route } from "react-router-dom"; //you write {BrowserRouter as Router}
 import Home from "./pages/Home"
 import About from "./pages/About"
 import NotFound from "./pages/NotFound"
 import Vans from "./pages/Vans"
 import VanDetails from "./pages/VanDetails"
+import Layout from './components/Layout';
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/vans" element={<Vans/>}/> {/* the route to the component*/}
-          <Route path="/vans/:id" element={<VanDetails/>}/>
-          <Route path="*" element={<NotFound/>} /> {/* path="*" Default for all non-declared path, you need to import it as component*/}
+          <Route element={<Layout/>}>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/vans" element={<Vans/>}/> {/* the route to the component*/}
+            <Route path="/vans/:id" element={<VanDetails/>}/>
+            <Route path="*" element={<NotFound/>} /> {/* path="*" Default for all non-declared path, you need to import it as component*/}
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
