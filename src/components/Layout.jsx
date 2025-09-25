aa9091e041a893f446bbb1dcd73816df7f2dea1c
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-09-25 13:44:04 +0200

    added Footer to the Layout and created a Footer component in the component (the common files folder) this means that Footer is never in touch with App.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout = () => {
   return (
-    <>
+    <>  
+        <main>
         <Header/>
         <Outlet/>
+        </main>
+        <Footer/>
     </>
   )
 }
