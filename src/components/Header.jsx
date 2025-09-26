0d155c66b1e982ab2918da28ceb318bcce6f1fa2
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-09-26 10:30:29 +0200

    added className to the main navigation too

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
+import { NavLink} from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
-      <Link className="site-logo" to="/">#VanLife</Link>
+      <NavLink to="/" className={({isActive}) => isActive ? "activenav site-logo" : "site-logo"}>#VanLife</NavLink>
       <nav>
-        <Link to="/host">Host</Link>
-        <Link to="/about">About</Link>
-        <Link to="/vans">Vans</Link>
+        <NavLink to="/host" className={({isActive}) => isActive ? "activenav" : null}>Host</NavLink>
+        <NavLink to="/about" className={({isActive}) => isActive ? "activenav" : null}>About</NavLink>
+        <NavLink to="/vans" className={({isActive}) => isActive ? "activenav" : null}>Vans</NavLink>
       </nav>
     </header>
   );
--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -5,15 +5,11 @@ const HostLayout = () => {
   return (
     <>
       <nav className="host-nav">
-        <NavLink 
-        to="/host"
-        end
-        className={({isActive}) => isActive ? "activenav" : null}        
+        <NavLink to="/host" end className={({isActive}) => isActive ? "activenav" : null}        
         >Dashboard</NavLink>
-        <NavLink to="/host/income"
-        className={({isActive}) => isActive ? "activenav" : null}
+        <NavLink to="/host/income" className={({isActive}) => isActive ? "activenav" : null}
         >Income</NavLink>
-        <NavLink to="/host/reviews">Reviews</NavLink>
+        <NavLink to="/host/reviews" className={({isActive}) => isActive ? "activenav" : null}>Reviews</NavLink>
       </nav>
       <Outlet />
     </>
