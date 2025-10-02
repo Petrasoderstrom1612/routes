bc9722d8af73f7d4054fa7505ae446f15f0fad71
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-10-02 14:59:09 +0200

    moved Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ function App() {
             <Route path="/" element={<Home/>}/>
 
             <Route path="host" element={<HostLayout/>}> {/* this acts yet as another layout that layers on the side, you need outlet not to override the children*/}
+              <Route index element={<Dashboard />} />
               <Route path="income" element={<Income/>}/>
               <Route path="reviews" element={<Reviews/>}/>
               <Route path="hostvans" element={<HostVans/>}/>  {/*not a parent as it does not share any UI with hostvans/:id*/}
@@ -35,7 +36,6 @@ function App() {
                 <Route path="pricing" element={<HostPricing/>}/>
                 <Route path="photos" element={<HostPhotos/>}/>
               </Route>
-              <Route index element={<Dashboard />} />
             </Route>
 
             <Route path="about" element={<About/>}/>
