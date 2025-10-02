e7e51f08fdb13cb01d5b12bebfce3c2276e84039
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-10-02 13:58:14 +0200

    adjusted comment about NavLinks

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -4,7 +4,7 @@ import { NavLink, Outlet } from "react-router-dom";
 const HostLayout = () => {
   return (
     <>
-      <nav className="host-nav">
+      <nav className="host-nav"> {/* the absolute paths can be turned it into relative without any difference */}
         <NavLink to="/host" end className={({isActive}) => isActive ? "activenav" : null}>Dashboard</NavLink>
         <NavLink to="/host/income" className={({isActive}) => isActive ? "activenav" : null}>Income</NavLink>
         <NavLink to="/host/hostvans" className={({isActive}) => isActive ? "activenav" : null}>Vans</NavLink>
