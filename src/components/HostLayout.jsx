25b470096c6760e40920b38cc3f2e4366f6a7b1a
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-10-02 14:51:35 +0200

    It is dryer to have Links to relative paths instead of absolute. In that way if one segment changes you do not need to manually correct it in every Link.  Since the index Route links to the same path as its Layout (see the image above). (Index page is case where you have page on the Outlet Link.) Remove the link to "/host" would mean that we link to the startpage and we do not want that. So the rule of thumb. In a Link to index route the to is always "."

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -4,11 +4,11 @@ import { NavLink, Outlet } from "react-router-dom";
 const HostLayout = () => {
   return (
     <>
-      <nav className="host-nav"> {/* the absolute paths can be turned it into relative without any difference */}
-        <NavLink to="/host" end className={({isActive}) => isActive ? "activenav" : null}>Dashboard</NavLink>
-        <NavLink to="/host/income" className={({isActive}) => isActive ? "activenav" : null}>Income</NavLink>
-        <NavLink to="/host/hostvans" className={({isActive}) => isActive ? "activenav" : null}>Vans</NavLink>
-        <NavLink to="/host/reviews" className={({isActive}) => isActive ? "activenav" : null}>Reviews</NavLink>
+      <nav className="host-nav"> 
+        <NavLink to="." end className={({isActive}) => isActive ? "activenav" : null}>Dashboard</NavLink>
+        <NavLink to="income" className={({isActive}) => isActive ? "activenav" : null}>Income</NavLink>
+        <NavLink to="hostvans" className={({isActive}) => isActive ? "activenav" : null}>Vans</NavLink>
+        <NavLink to="reviews" className={({isActive}) => isActive ? "activenav" : null}>Reviews</NavLink>
       </nav>
       <Outlet />
     </>
