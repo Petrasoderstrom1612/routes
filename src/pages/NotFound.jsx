186fe70d1200cb6081fb773929f683fc19429949
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-10-14 17:01:29 +0200

    better CSS to No found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>Not found</h1>
-    </div>
+        <div className="not-found-container">
+            <h1>Sorry, the page you were looking for was not found.</h1>
+            <Link to="/" className="link-button">Return to Home</Link>
+        </div>
   )
 }
 
