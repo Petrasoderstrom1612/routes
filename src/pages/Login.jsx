c8d59af2c433e360aac1bfb0bac0ae2eb52ef7fa
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-10-16 15:50:58 +0200

    added a log in form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 
 const Login = () => {
+    const [loginFormData, setLoginFormData] = React.useState({email: "", password:""})
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(loginFormData)
+    }
+
+    const handleChange = (e) => {
+        const {name, value} =e.target
+        setLoginFormData(prev => {
+            return{
+                ...prev, [name]: value
+            }
+        })
+    }
+
   return (
-    <div>
-      login
+    <div className="login-container">
+        <h1>Sign in to your account</h1>
+        <form onSubmit={handleSubmit} classname="login-form">
+            <input
+                name="email" 
+                onChange={handleChange} 
+                type="email"
+                value={loginFormData.email}
+                placeholder="Email address"          
+            />
+            <input
+                name="password"
+                type="password"
+                onChange={handleChange}
+                value={loginFormData.password}
+                placeholder="Password"
+            />
+            <button>Log in</button>
+        </form>
     </div>
   )
 }
