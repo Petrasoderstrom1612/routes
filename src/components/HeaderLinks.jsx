c9d94ba0ee8051bdf9f457abc22b079498c90b24
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-10-16 15:18:24 +0200

    added react icon snd installed  npm install react-icons

--- src/components/HeaderLinks.jsx
+++ src/components/HeaderLinks.jsx
@@ -1,4 +1,5 @@
 import { NavLink} from "react-router-dom";
+import { RxAvatar } from "react-icons/rx";
 
 const HeaderLinks = () => {
   return (
@@ -8,7 +9,7 @@ const HeaderLinks = () => {
         <NavLink to="host" className={({isActive}) => isActive ? "activenav" : null}>Host</NavLink>
         <NavLink to="about" className={({isActive}) => isActive ? "activenav" : null}>About</NavLink>
         <NavLink to="vans" className={({isActive}) => isActive ? "activenav" : null}>Vans</NavLink>
-        <NavLink to="login" className={({isActive}) => isActive ? "activenav" : null}>Login</NavLink>
+        <NavLink to="login" className={({isActive}) => isActive ? "activenav" : null}><RxAvatar /></NavLink>
       </nav>
     </header>
   );
