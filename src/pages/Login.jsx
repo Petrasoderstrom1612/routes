f5bd2cce353ab4fa5c923aa2af80a67677512977
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-10-16 16:12:24 +0200

    final form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,43 +2,31 @@ import React from 'react'
 
 const Login = () => {
     const [loginFormData, setLoginFormData] = React.useState({email: "", password:""})
-    
-    // const handleSubmit = (e) => {
-        //     e.preventDefault()
-        //     console.log(loginFormData)
-        // }
         
-        const handleSubmit = (formData) => {
+        const handleSubmit = (e) => {
+            e.preventDefault()
+            const formData = new FormData(e.target)
             const data = Object.fromEntries(formData)
+            setLoginFormData(data)
             console.log(data)
-        }
-        
-        
-        const handleChange = (e) => {
-            const {name, value} =e.target
-            setLoginFormData(prev => {
-                return{
-                    ...prev, [name]: value
-                }
-            })
         }
     
   return (
     <div className="login-container">
         <h1>Sign in to your account</h1>
-        <form method="post" action={handleSubmit} classname="login-form">
+        <form method="post" onSubmit={handleSubmit} className="login-form">
             <input
                 name="email" 
-                onChange={handleChange} 
+                // onChange={handleChange} 
                 type="email"
-                value={loginFormData.email}
+                // value={loginFormData.email}
                 placeholder="Email address"          
             />
             <input
                 name="password"
                 type="password"
-                onChange={handleChange}
-                value={loginFormData.password}
+                // onChange={handleChange}
+                // value={loginFormData.password}
                 placeholder="Password"
             />
             <button>Log in</button>
